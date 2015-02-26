cc6b916d876f404aad21c4757f09954db4079b25
ducreyna <ducrey.nathan@gmail.com>
2015-02-26 09:13:17 +0100

    [Update] Can get documents from its id

--- test/CouchbaseSmart.js
+++ test/CouchbaseSmart.js
@@ -15,44 +15,49 @@ var config = {
 var couchbase = new CouchbaseSmart(config);
 
 describe('Couchbase smart interface', function () {
-  var key = 'myKey';
-  var data = 'I_AM_DATA';
 
-  it('Set a new document with a bad key', function (done) {
-    couchbase.insert(1, data, function (err) {
-      // Asserts
-      assert.equal(err.message, "Key must be a String");
-      done();
+  describe('Basic operations', function () {
+    var key = 'myKey';
+    var data = 'I_AM_DATA';
+    var id = '';
+
+    it('Set a new document with a bad key', function (done) {
+      couchbase.insert(1, data, function (err) {
+        // Asserts
+        assert.equal(err.message, "Key must be a String");
+        done();
+      });
     });
-  });
 
-  it('Set a new document', function (done) {
-    couchbase.insert(key, data, function (err, docId) {
-      // Asserts
-      assert.ifError(err);
-      assert.equal(typeof(docId), 'string');
-      done();
+    it('Set a new document', function (done) {
+      couchbase.insert(key, data, function (err, docId) {
+        // Asserts
+        assert.ifError(err);
+        assert.equal(typeof(docId), 'string');
+        id = docId;
+        done();
+      });
     });
-  });
 
-  it('Get all documents from a key', function (done) {
-    couchbase.get(key, {}, function (err, result) {
-      // Asserts
-      assert.ifError(err);
-      assert.ok(result.length > 0);
-      assert.equal(typeof(result[0]), 'object');
-      assert.equal(typeof(result[0].time), 'number');
-      assert.equal(typeof(result[0].id), 'string');
-      done();
+    it('Get document from its key', function (done) {
+      couchbase.get(key, {}, function (err, result) {
+        // Asserts
+        assert.ifError(err);
+        assert.equal(result.length, 1);
+        assert.equal(typeof(result[0]), 'object');
+        assert.equal(typeof(result[0].time), 'number');
+        assert.equal(typeof(result[0].id), 'string');
+        done();
+      });
     });
-  });
 
-  it('Remove all documents from a key', function (done) {
-    couchbase.remove(key, {}, function (err, nbDeletions) {
-      // Asserts
-      assert.ifError(err);
-      assert.ok(nbDeletions > 0);
-      done();
+    it('Remove document from its docId', function (done) {
+      couchbase.remove(key, { 'ids': [id] }, function (err, nbDeletions) {
+        // Asserts
+        assert.ifError(err);
+        assert.equal(nbDeletions, 1);
+        done();
+      });
     });
   });
 });
